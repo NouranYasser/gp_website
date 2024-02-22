a05140995c6b6bec396bd052ff82e88ff21439c9
Hoalfatih <hodaalfatih2020@gmail.com>
2024-02-22 04:47:26 +0200

    add

--- src/Admin/Baby/TipsAndActivitiesMonth21.js
+++ src/Admin/Baby/TipsAndActivitiesMonth21.js
@@ -45,7 +45,7 @@ const TipsAndActivitiesMonth21 = () => {
 
   return (
     <>
-      <div>
+      <div className="bmo">
         {tips.err && <p>{tips.err}</p>}
         {tips.loading ? (
           <p>Loading...</p>
