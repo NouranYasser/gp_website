3e999fe9220682606de3e95ffaeb1353a0e33242
NouranYasser <nouranyasser366@gmail.com>
2024-05-31 16:29:23 +0300

    Final App

--- src/Admin/Baby/ProductCard.js
+++ src/Admin/Baby/ProductCard.js
@@ -42,15 +42,7 @@ function ProductCard() {
         <div key={item.id} className="product-Card">
           <div className="card-top"></div>
           <div className="card-info">
-<<<<<<< HEAD
             <h3 className="title">{item.name}</h3>
-            
-=======
-          <button onClick={() => deleteIssue(item.id)}>
-          <FontAwesomeIcon icon={faTrash} />
-        </button>
-            <h3 className="title">{item.name}</h3>  
->>>>>>> e29f2e91daad5bc151029131f3d162a5f0bf9ce6
             <Link to={"/ShowDetails/" + item.id}>
               <button className="btnShow">Show Details</button>
             </Link>
