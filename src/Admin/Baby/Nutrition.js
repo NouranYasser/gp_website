d0387fa4b30ac6fc285b23e909cfb4774e070191
Hoalfatih <hodaalfatih2020@gmail.com>
2024-06-03 01:40:34 +0300

    add

--- src/Admin/Baby/Nutrition.js
+++ src/Admin/Baby/Nutrition.js
@@ -1,32 +1,32 @@
 import React, { useState, useEffect } from "react";
  import { Link } from "react-router-dom";
  import axios from "axios";
- import "../../style/food.css";
+ import "../../style/nutration.css";
 
 
 const Nutrition = () => {
  
     return (
       <>  
-      <div className='f_foot'>
+      <div className='f_foot_N'>
         <br></br>
         <br></br>
-<h1 className="f-p">Nutrition </h1>
+<h1 className="f-pN">Nutrition </h1>
 <br></br>
 <br></br>
 <br></br>
 <br></br> 
-<div className="fo-container">
+<div className="fo-containerN">
 <Link to="/Weaning">
-        <button className="fo-b">Weaning</button>
+        <button className="fo-bN">Weaning</button>
       </Link> 
 
       <Link to="/BreastFeeding">
-        <button className="fo-b">Breast Feeding</button>
+        <button className="fo-bN">Breast Feeding</button>
       </Link>  
 
        <Link to="/ArtificalFeeding">
-        <button className="fo-b">Artifical Feeding</button>
+        <button className="fo-bN">Artifical Feeding</button>
         </Link>  
 
         </div>
