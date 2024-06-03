4297d200fb5ce5b7635912af84bdc8f987862471
MariamHossam565 <mariamhossam539@gmail.com>
2024-06-03 08:23:04 -0700

    last

--- src/Admin/Baby/TipsAndActivities.js
+++ src/Admin/Baby/TipsAndActivities.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../../style/food.css";
+//import "../../style/food.css";
 import "../../style/tips.css";
 
 const TipsAndActivities = () => {
@@ -17,34 +17,34 @@ const TipsAndActivities = () => {
       <div className="TA-container">
         <div>
           <Link to="/TipsAndActivitiesMonth1">
-            <button className="fo-b"> Month 1-3</button>
+            <button className="fo-bbb"> Month 1-3</button>
           </Link>
 
           <Link to="/TipsAndActivitiesMonth9">
-            <button className="fo-b"> Month 9-12</button>
+            <button className="fo-bbb"> Month 9-12</button>
           </Link>
           <Link to="/TipsAndActivitiesMonth18">
-            <button className="fo-b"> Month 18-21</button>
+            <button className="fo-bbb"> Month 18-21</button>
           </Link>
         </div>
         <div>
           <Link to="/TipsAndActivitiesMonth3">
-            <button className="fo-b"> Month 3-6</button>
+            <button className="fo-bbb"> Month 3-6</button>
           </Link>
 
           <Link to="/TipsAndActivitiesMonth12">
-            <button className="fo-b"> Month 12-15</button>
+            <button className="fo-bbb"> Month 12-15</button>
           </Link>
           <Link to="/TipsAndActivitiesMonth21">
-            <button className="fo-b"> Month 21-24</button>
+            <button className="fo-bbb"> Month 21-24</button>
           </Link>
         </div>
         <div className="last">
           <Link to="/TipsAndActivitiesMonth6">
-            <button className="fo-b"> Month 6-9</button>
+            <button className="fo-bbb"> Month 6-9</button>
           </Link>
           <Link to="/TipsAndActivitiesMonth15">
-            <button className="fo-b"> Month 15-18</button>
+            <button className="fo-bbb"> Month 15-18</button>
           </Link>
         </div>
       </div>
--- src/Admin/Baby/Weaning.js
+++ src/Admin/Baby/Weaning.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
  import { Link } from "react-router-dom";
  import axios from "axios";
- import "../../style/food.css";
+ import "../../style/foodBaby.css";
 
 
 const Weaning = () => {
  
     return (
       <>
-    <div className="f_foot">
+    <div className="fb_foot">
 <br></br>
 <br></br>
 <br></br>
@@ -24,22 +24,22 @@ const Weaning = () => {
 <br></br>
 <br></br>
 <br></br> 
-<div className="fo-container">
+<div className="fob-container">
       <Link to="/WeaningMonth1">
-        <button className="fo-b"> Month 1-6</button>
+        <button className="fo-baby"> Month 1-6</button>
       </Link> 
 
       <Link to="/WeaningMonth6">
-        <button className="fo-b"> Month 6-12</button>
+        <button className="fo-baby"> Month 6-12</button>
       </Link>  
 
 
       <Link to="/WeaningMonth12">
-        <button className="fo-b"> Month 12-18</button>
+        <button className="fo-baby"> Month 12-18</button>
       </Link> 
 
       <Link to="/WeaningMonth18">
-        <button className="fo-b"> Month 18-24</button>
+        <button className="fo-baby"> Month 18-24</button>
       </Link>
       </div>
 
